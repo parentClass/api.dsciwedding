8bbaabfcf30c76fa9ab4df891dc2b9941e97e352
parentClass <dimalantadaniel@gmail.com>
2024-07-02 01:19:18 +0800

    added rsvp list route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,4 @@ $routes->get('/faqs/list', 'Faq::list', ['filter' => 'cors']);
 $routes->get('/faqs/(:alpha)', 'Faq::faqLookupByCategory/$1', ['filter' => 'cors']);
 
 $routes->post('/rsvp', 'Rsvp::record', ['filter' => 'cors']);
+$routes->get('/rsvp/list', 'Rsvp::list', ['filter' => 'cors']);
