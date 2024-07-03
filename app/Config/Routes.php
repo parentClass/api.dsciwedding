5f6cb10a8d4d9c6db0563b4462e3b411f85edcbe
parentClass <dimalantadaniel@gmail.com>
2024-07-04 00:36:15 +0800

    updated template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,11 +6,14 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
+// faqs
 $routes->get('/faqs/list', 'Faq::list', ['filter' => 'cors']);
 $routes->get('/faqs/(:alpha)', 'Faq::faqLookupByCategory/$1', ['filter' => 'cors']);
 
+// rsvp
 $routes->post('/rsvp', 'Rsvp::record', ['filter' => 'cors']);
 $routes->get('/rsvp/list', 'Rsvp::list', ['filter' => 'cors']);
 $routes->post('/rsvp/approve', 'Rsvp::approve', ['filter' => 'cors']);
 
+// invitation
 $routes->get('/download/invitation', 'Home::download', ['filter' => 'cors']);
