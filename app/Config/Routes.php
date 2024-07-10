e804f0e01275791b3e2f736e6ea0b7be2216098b
parentClass <dimalantadaniel@gmail.com>
2024-07-10 23:42:27 +0800

    added decline endpoint

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,6 +15,7 @@ $routes->post('/rsvp', 'Rsvp::record', ['filter' => 'cors']);
 $routes->get('/rsvp/list', 'Rsvp::list', ['filter' => 'cors']);
 $routes->post('/rsvp/approve', 'Rsvp::approve', ['filter' => 'cors']);
 $routes->get('/rsvp/stats', 'Rsvp::stats', ['filter' => 'cors']);
+$routes->post('/rsvp/decline', 'Rsvp::decline', ['filter' => 'cors']);
 
 // invitation
 $routes->get('/download/invitation', 'Home::download', ['filter' => 'cors']);
