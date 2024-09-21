ecbf960237b05990fc2a14de48c7e2dffd692b89
parentClass <dimalantadaniel@gmail.com>
2024-09-21 14:31:15 +0800

    fix error in gallery list

--- app/Controllers/Gallery.php
+++ app/Controllers/Gallery.php
@@ -22,10 +22,10 @@ class Gallery extends BaseController
         foreach($galleries as $gallery) {
             $img = 'https://api.dsciwedding.com/uploads/' . $gallery['category'] . '/' . $gallery['file_name'];
 
-            array_push($categorizedGallery[$gallery['category']], {
+            array_push($categorizedGallery[$gallery['category']], [
                 "src" => $img,
-                "thumbnail" => generateThumbnail($img, 100, 50, 65);
-            });
+                "thumbnail" => generateThumbnail($img, 100, 50, 65)
+            ]);
         }
 
         return json_encode($categorizedGallery);
@@ -117,7 +117,7 @@ class Gallery extends BaseController
             $imagick->setImageCompressionQuality($quality);
             $imagick->thumbnailImage($width, $height, false, false);
             $filename_no_ext = reset(explode('.', $img));
-            
+
             if (file_put_contents($filename_no_ext . '_thumb' . '.jpg', $imagick) === false) {
                 throw new Exception("Could not put contents.");
             }
